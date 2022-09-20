87858b664ba4d07d7a85f6a466d7b7dcf4823053
Aman Mamgain <amanmamgain9@gmail.com>
2022-09-20 15:26:38 +0530

    adding show/hide wallet buttons

--- src/App.js
+++ src/App.js
@@ -8,34 +8,50 @@ function App() {
 
   useEffect(() => {
     const initWallet = async () => {
-      const wallet = await getGlipWallet();
-      setWallet(wallet);
+        const wallet = await getGlipWallet();
+        setWallet(wallet);
+        console.log('userinfo', await wallet.getUserInfo());
+        console.log('www');
     };
     initWallet();
   }, []);
 
-  return (
-    <div className="App">
-      <div id="glip-wallet"></div>
-
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={() => wallet.showConnectModal(['google'])}>modeal</button>
-      </header>
-    </div>
-  );
+    const signTransaction = async () =>{
+        let signer = await wallet.getSigner();
+        let displayMessage = "This transaction transfers 0 value";
+        let publicAddress = (await wallet.getUserInfo()).publicAddress;
+        let signedTx = signer.signTransaction({
+            to: '0x0000000000000000000000000000000000000000',
+            value: '0x0',
+            data: '0x0',
+            chainId: 137,
+            nonce: 0,
+            gasPrice: 0,
+            gasLimit: 0,
+            from: publicAddress,
+        }, displayMessage);
+    }
+    return (
+        <div className="App">
+          <div id="glip-wallet"></div>
+          <header className="App-header">
+            <button onClick={() => wallet.showConnectModal(['google'])}>Show Modal</button>
+            <button onClick={async () => {
+                const loggedOut = await wallet.logout();
+                console.log('loggedOut', loggedOut);
+            }}>logout</button>
+            <button onClick={async () => {
+                signTransaction();
+            }}>Sign Example Transaction</button>
+            <button onClick={async () => {
+                wallet.showWallet()
+            }}>Show Wallet</button>
+            <button onClick={async () => {
+                wallet.hideWallet()
+            }}>Hide Wallet</button>
+          </header>
+        </div>
+    );
 }
 
 export default App;
